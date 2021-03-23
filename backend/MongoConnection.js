fc1d317b54abadcb4b05a5b49e5aee8da32f7e57
Seth Kimmel <seth.kimmel3@gmail.com>
2021-03-23 02:36:03 +0000

    updates

--- backend/MongoConnection.js
+++ backend/MongoConnection.js
@@ -1,19 +1,23 @@
 const MongoClient = require('mongodb').MongoClient
-const url = 'mongodb://127.0.0.1:27017/';
+const url = 'mongodb://db:27017/';
+//const url = 'mongodb://127.0.0.1:27017/';
 
-var _db; 
+var _db;
 
-module.exports = { 
+module.exports = {
     connectToServer: async function ( database ){
+        // url += database;
         await MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
-            _db = client.db(database);
+            	if(err) console.log(err);
+		_db = client.db(database);
+		console.log("Mongo connected");
         })
-    }, 
-    
+    },
+
     returnConnectionPromise: async function (database){
         return await MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     },
-    
+
     getDb: function(){
         return _db;
     }
