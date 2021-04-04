c69e39d79023d6af73667f222168af50369d3dc1
Seth Kimmel <seth.kimmel3@gmail.com>
2021-04-04 17:01:17 -0500

    clean up songbird

--- backend/songbird.js
+++ backend/songbird.js
@@ -9,23 +9,28 @@ var DBUtils = require('./DBUtils.js');
 var cron = require('node-cron');
 var Twit = require('twit')
 
-mongoUtil.connectToServer("discourseListDatabase", function(err, client){
+mongoUtil.connectToServer("roundtableListDatabase", function(err, client){
     if (err) console.log(err);
 });
 
+const {
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET
+} = process.env
 
 var T = new Twit({
-  consumer_key:         'RwXnCUM4ZrDm9zeMtaXFpYOfb',
-  consumer_secret:      '5P9E8UTXeerWDhZm7D9z4bkgVtoU43vBFXdkt3mV7bEJGHTFpl',
-  access_token:         '1323445323101052935-frMgvus9pwALWfywtLrVIqhKLr0ojB',
-  access_token_secret:  'osKy4OgOQAFhRPnt32Xi4N1d9eqtjxSvXUe8RjJz90KiU',
+  consumer_key:         CONSUMER_KEY, //'RwXnCUM4ZrDm9zeMtaXFpYOfb'
+  consumer_secret:      CONSUMER_SECRET, //'5P9E8UTXeerWDhZm7D9z4bkgVtoU43vBFXdkt3mV7bEJGHTFpl'
+  access_token:         ACCESS_TOKEN, //'1323445323101052935-frMgvus9pwALWfywtLrVIqhKLr0ojB'
+  access_token_secret:  ACCESS_TOKEN_SECRET, //'osKy4OgOQAFhRPnt32Xi4N1d9eqtjxSvXUe8RjJz90KiU'
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
 
 var post_tweet = (name, description, tags, public_join, public_listen, UDI) => {
     try{
-
         var tags_string = '';
         for(var i = 0; i < tags.length; i++){
             tags_string += '#' + tags[i].replace(/\s/g,'');
@@ -48,7 +53,7 @@ var post_tweet = (name, description, tags, public_join, public_listen, UDI) => {
 
         var public_availability_string = 'Public Join: ' + join_emoji + ' | Public Listen: ' + listen_emoji + '\n';
 
-        var status = 'DISCOURSE BOT 🤖: \n';
+        var status = 'ROUNDTABLE BOT 🤖: \n';
         status += 'Name: ' + name + '\n';
         // (280 - 18 - UDI.length will ensure there are enough chars remaining for link)
         if((status.length + (13 + description.length) < (280 - 18 - UDI.length))){
@@ -60,7 +65,7 @@ var post_tweet = (name, description, tags, public_join, public_listen, UDI) => {
         if((status.length + public_availability_string.length) < (280 - 18 - UDI.length)){
             status += public_availability_string;
         }
-        status += 'discourse.fm/live/' + UDI;
+        status += 'roundtable.audio/live/' + UDI;
 
         T.post('statuses/update', { status: status }, function(err, data, response){
           return data['id'];
@@ -86,19 +91,19 @@ var post_tweet = (name, description, tags, public_join, public_listen, UDI) => {
 //            }
 //        };
 //
-//        var cursor = dbo.collection("discourseList").find(ongoing_public_query);
+//        var cursor = dbo.collection("roundtableList").find(ongoing_public_query);
 //        cursor.each(function(err,item){
 //            if(item != null){
 //
 //                // find if tweet already made
 //                var _id = item['_id'];
 //                var tweet_exists_query = {
-//                    discourse_id: {
+//                    roundtable_id: {
 //                        $eq: _id
 //                    }
 //                }
 //
-//                dbo.collection("discourseTweets").find(tweet_exists_query).count().then(function(numItems){
+//                dbo.collection("roundtableTweets").find(tweet_exists_query).count().then(function(numItems){
 //                    if(numItems == 0){
 //                        // tweet it
 //                        var name = item['name'];
@@ -111,12 +116,12 @@ var post_tweet = (name, description, tags, public_join, public_listen, UDI) => {
 //                        var tweet_id = post_tweet(name, description, tags, public_join, public_listen, UDI);
 //
 //                        if(tweet_id != 'error'){
-//                            //insert into discourseTweets collection
+//                            //insert into roundtableTweets collection
 //                            var to_insert = {
-//                                'discourse_id': _id,
+//                                'roundtable_id': _id,
 //                                'tweet_id': tweet_id
 //                            }
-//                            dbo.collection("discourseTweets").insertOne(to_insert, function(e, res){
+//                            dbo.collection("roundtableTweets").insertOne(to_insert, function(e, res){
 //                                if(e) throw e;
 //                                console.log('new tweet posted');
 //                            });
@@ -151,7 +156,7 @@ function newMention(tweet){
             var is_retweet = false;
         }
 
-        if(tweettxt.includes('start a discourse') && is_retweet == false){
+        if(tweettxt.includes('start a roundtable') && is_retweet == false){
 
             var sender_name = tweet.user.screen_name;
             var tweet_id = tweet.id_str;
@@ -167,23 +172,23 @@ function newMention(tweet){
                 var tweet_link = 'twitter.com/' + sender_name + '/status/' + original_tweet_in_reply_to;
             }
 
-            var discourse_info_array = {
-                                           "discourse-name": 'A Freshly Whipped up Discourse for @' + sender_name,
-                                           "discourse-description": "In response to: " + tweet_link,
-                                           "discourse-tags": ['autogenerated', 'from', 'tweet'],
+            var roundtable_info_array = {
+                                           "roundtable-name": 'A Freshly Whipped up Roundtable for @' + sender_name,
+                                           "roundtable-description": "In response to: " + tweet_link,
+                                           "roundtable-tags": ['autogenerated', 'from', 'tweet'],
                                            "join-visibility": 'public',
                                            "listen-visibility": 'public',
-                                           "discourse_JID": null,
-                                           "discourse_LID": null,
+                                           "roundtable_JID": null,
+                                           "roundtable_LID": null,
                                            "max-allowed-participants": 100
                                         }
 
-            DBUtils.callCreateNewDiscoursePromise(discourse_info_array).then(function(res){
+            DBUtils.callCreateNewRoundtablePromise(roundtable_info_array).then(function(res){
 //                console.log(res);
                 if("UDI" in res){
                     //successly created
                     var UDI = res["UDI"];
-                    var reply_link = 'discourse.fm/live/' + UDI;
+                    var reply_link = 'roundtable.audio/live/' + UDI;
 
                     if(in_reply_to_screen_name == null){
                         var reply = "Here's a newly created audio chat room for this tweet @" + sender_name + ".\n\n" + reply_link + "\n\nCome discuss live with others!";
@@ -204,7 +209,7 @@ function newMention(tweet){
                        }
                     });
                 }else{
-                    console.log("Error in creating discourse: " + res);
+                    console.log("Error in creating roundtable: " + res);
                 }
             })
         }
