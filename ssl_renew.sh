33fa4645f4664ca416c7c30b35582d2918f877a4
Seth Kimmel <seth.kimmel3@gmail.com>
2021-04-15 03:34:31 +0000

    final touches (hopefully)

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /home/seth/discourse.fm/
+cd /home/seth/roundtable.audio/
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
